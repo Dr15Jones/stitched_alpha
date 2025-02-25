cms-sw/cmssw

Implement edm::ProductNamePattern
Implement a type that can be used to match product names based on their
friendly type, module name, instance label and process name.

DataFormats/Provenance/interface/ProductNamePattern.h
@@ -1,0 +1,66 @@
+#ifndef DataFormats_Provenance_interface_ProductNamePattern_h
+#define DataFormats_Provenance_interface_ProductNamePattern_h
+
+#include <string>
+#include <regex>
+#include <vector>
+
+#include "DataFormats/Provenance/interface/ProductDescription.h"
+
+namespace edm {
+
+  /* ProductNamePattern
+   *
+   * A ProductNamePattern is constructed from a string representing either a module label (e.g. "<module label>") or a
+   * a branch name (e.g. "<product type>_<module label>_<instance name>_<process name>").
+   *
+   * A ProductNamePattern object can be compared with a ProductDescription object using the match() method:
+   *
+   *     branchPattern.match(branch)
+   *
+   * .
+   * Glob expressions ("?" and "*") are supported in module labels and within the individual fields of branch names,
+   * similar to an OutputModule's "keep" statements.
+   * Use "*" to match all products of a given category.
+   *
+   * If a module label is used, it must not contain any underscores ("_"); the resulting ProductNamePattern will match all
+   * the branches prodced by a module with the given label, including those with a non-empty instance names, and those
+   * produced by the Transformer functionality (such as the implicitly copied-to-host products in case of Alpaka-based
+   * modules).
+   * If a branch name is used, all four fields must be present, separated by underscores; the resulting ProductNamePattern
+   * will match the branches matching all four fields.
+   *
+   * For example, in the case of products from an Alpaka-based producer running on a device
+   *
+   *     ProductNamePattern("module")
+   *
+   * would match all branches produced by "module", including the automatic host copy of its device products.
+   * While
+   *
+   *     ProductNamePattern( "*DeviceProduct_module_*_*" )
+   *
+   * would match only the branches corresponding to the device products.
+   */
+
+  class ProductNamePattern {
+  public:
+    explicit ProductNamePattern(std::string const& label);
+
+    bool match(edm::ProductDescription const& branch) const;
+
+  private:
+    std::regex type_;
+    std::regex moduleLabel_;
+    std::regex productInstanceName_;
+    std::regex processName_;
+  };
+
+  /* branchPatterns
+   *
+   * Utility function to construct a vector<edm::ProductNamePattern> from a vector<std::string>.
+   */
+  std::vector<ProductNamePattern> branchPatterns(std::vector<std::string> const& labels);
+
+}  // namespace edm
+
+#endif  // DataFormats_Provenance_interface_ProductNamePattern_h

DataFormats/Provenance/src/ProductNamePattern.cc
@@ -1,0 +1,104 @@
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <regex>
+#include <vector>
+
+#include <boost/algorithm/string/replace.hpp>
+
+#include "DataFormats/Provenance/interface/ProductNamePattern.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace edm {
+
+  /* glob_to_regex
+   *
+   * Utility function to convert a shell-like glob expression to a regex.
+   */
+  static std::regex glob_to_regex(std::string pattern) {
+    boost::replace_all(pattern, "*", ".*");
+    boost::replace_all(pattern, "?", ".");
+    return std::regex(pattern);
+  }
+
+  /* ProductNamePattern
+   *
+   * A ProductNamePattern is constructed from a string representing either a module label (e.g. "<module label>") or a
+   * a branch name (e.g. "<product type>_<module label>_<instance name>_<process name>").
+   *
+   * See DataFormats/Provenance/interface/ProductNamePattern.h for more details.
+   */
+  ProductNamePattern::ProductNamePattern(std::string const& label) {
+    static constexpr char kSeparator = '_';
+    static constexpr std::string_view kWildcard{"*"};
+    static const std::regex kAny{".*"};
+    static const std::regex kFields{"([a-zA-Z0-9*]+)_([a-zA-Z0-9_*]+)_([a-zA-Z0-9*]*)_([a-zA-Z0-9*]+)"};
+
+    // empty label
+    if (label.empty()) {
+      throw edm::Exception(edm::errors::Configuration) << "Invalid module label or branch name: \"" << label << "\"";
+    }
+
+    // wildcard
+    if (label == kWildcard) {
+      type_ = kAny;
+      moduleLabel_ = kAny;
+      productInstanceName_ = kAny;
+      processName_ = kAny;
+      return;
+    }
+
+    int underscores = std::count(label.begin(), label.end(), kSeparator);
+    if (underscores == 0) {
+      // Convert the module label into a regular expression.
+      type_ = kAny;
+      moduleLabel_ = glob_to_regex(label);
+      productInstanceName_ = kAny;
+      processName_ = kAny;
+      return;
+    }
+    
+    if (underscores >= 3) {
+      // Split the branch name into <product type>_<module label>_<instance name>_<process name>
+      // and convert the glob expressions into regular expressions.
+      // Note that:
+      //   - the <instance name> may be empty;
+      //   - for non-persistable branches, <module label> may contain additional underscores.
+      std::smatch fields;
+      if (std::regex_match(label, fields, kFields)) {
+        type_ = glob_to_regex(fields[1]);
+        moduleLabel_ = glob_to_regex(fields[2]);
+        productInstanceName_ = glob_to_regex(fields[3]);
+        processName_ = glob_to_regex(fields[4]);
+        return;
+      }
+    }
+
+    // Invalid input.
+    throw edm::Exception(edm::errors::Configuration) << "Invalid module label or branch name: \"" << label << "\"";
+  }
+
+  /* ProductNamePattern::match
+   *
+   * Compare a ProductNamePattern object with a ProductDescription object.
+   */
+  bool ProductNamePattern::match(edm::ProductDescription const& branch) const {
+    return (std::regex_match(branch.friendlyClassName(), type_) and
+            std::regex_match(branch.moduleLabel(), moduleLabel_) and
+            std::regex_match(branch.productInstanceName(), productInstanceName_) and
+            std::regex_match(branch.processName(), processName_));
+  }
+
+  /* branchPatterns
+   *
+   * Utility function to construct a vector<edm::ProductNamePattern> from a vector<std::string>.
+   */
+  std::vector<ProductNamePattern> branchPatterns(std::vector<std::string> const& labels) {
+    std::vector<ProductNamePattern> patterns;
+    patterns.reserve(labels.size());
+    for (auto const& label : labels)
+      patterns.emplace_back(label);
+    return patterns;
+  }
+
+}  // namespace edm

Dr15Jones/stitched_alpha

DataFormats/Provenance/interface/ProductNamePattern.h
@@ -1,0 +1,67 @@
+#ifndef DataFormats_Provenance_interface_ProductNamePattern_h
+#define DataFormats_Provenance_interface_ProductNamePattern_h
+
+#include <string>
+#include <regex>
+#include <vector>
+
+#include "DataFormats/Provenance/interface/ProductDescription.h"
+
+namespace edm {
+
+  /* ProductNamePattern
+   *
+   * A ProductNamePattern is constructed from a string representing either a module label (e.g. "<module label>") or a
+   * a branch name (e.g. "<product type>_<module label>_<instance name>_<process name>").
+   *
+   * A ProductNamePattern object can be compared with a ProductDescription object using the match() method:
+   *
+   *     productPattern.match(product)
+   *
+   * .
+   * Glob expressions ("?" and "*") are supported in module labels and within the individual fields of branch names,
+   * similar to an OutputModule's "keep" statements.
+   * Use "*" to match all products of a given category.
+   *
+   * If a module label is used, it must not contain any underscores ("_"); the resulting ProductNamePattern will match all
+   * the branches prodced by a module with the given label, including those with a non-empty instance names, and those
+   * produced by the Transformer functionality (such as the implicitly copied-to-host products in case of Alpaka-based
+   * modules).
+   * If a branch name is used, all four fields must be present, separated by underscores; the resulting ProductNamePattern
+   * will match the branches matching all four fields.
+   * Only in this case and only for transient products, the module label may contain additional underscores.
+   *
+   * For example, in the case of products from an Alpaka-based producer running on a device
+   *
+   *     ProductNamePattern("module")
+   *
+   * would match all branches produced by "module", including the automatic host copy of its device products.
+   * While
+   *
+   *     ProductNamePattern( "*DeviceProduct_module_*_*" )
+   *
+   * would match only the branches corresponding to the device products.
+   */
+
+  class ProductNamePattern {
+  public:
+    explicit ProductNamePattern(std::string const& label);
+
+    bool match(edm::ProductDescription const& product) const;
+
+  private:
+    std::regex type_;
+    std::regex moduleLabel_;
+    std::regex productInstanceName_;
+    std::regex processName_;
+  };
+
+  /* productPatterns
+   *
+   * Utility function to construct a vector<edm::ProductNamePattern> from a vector<std::string>.
+   */
+  std::vector<ProductNamePattern> productPatterns(std::vector<std::string> const& labels);
+
+}  // namespace edm
+
+#endif  // DataFormats_Provenance_interface_ProductNamePattern_h

DataFormats/Provenance/src/ProductNamePattern.cc
@@ -1,0 +1,106 @@
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <regex>
+#include <vector>
+
+#include <boost/algorithm/string/replace.hpp>
+
+#include "DataFormats/Provenance/interface/ProductNamePattern.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace edm {
+
+  /* glob_to_regex
+   *
+   * Utility function to convert a shell-like glob expression to a regex.
+   */
+  static std::regex glob_to_regex(std::string pattern) {
+    boost::replace_all(pattern, "*", ".*");
+    boost::replace_all(pattern, "?", ".");
+    return std::regex(pattern);
+  }
+
+  /* ProductNamePattern
+   *
+   * A ProductNamePattern is constructed from a string representing either a module label (e.g. "<module label>") or a
+   * a branch name (e.g. "<product type>_<module label>_<instance name>_<process name>").
+   *
+   * For transient products, the module label may contain additional underscores.
+   *
+   * See DataFormats/Provenance/interface/ProductNamePattern.h for more details.
+   */
+  ProductNamePattern::ProductNamePattern(std::string const& label) {
+    static constexpr char kSeparator = '_';
+    static constexpr std::string_view kWildcard{"*"};
+    static const std::regex kAny{".*"};
+    static const std::regex kFields{"([a-zA-Z0-9*]+)_([a-zA-Z0-9_*]+)_([a-zA-Z0-9*]*)_([a-zA-Z0-9*]+)"};
+
+    // empty label
+    if (label.empty()) {
+      throw edm::Exception(edm::errors::Configuration) << "Invalid module label or branch name: \"" << label << "\"";
+    }
+
+    // wildcard
+    if (label == kWildcard) {
+      type_ = kAny;
+      moduleLabel_ = kAny;
+      productInstanceName_ = kAny;
+      processName_ = kAny;
+      return;
+    }
+
+    int underscores = std::count(label.begin(), label.end(), kSeparator);
+    if (underscores == 0) {
+      // Convert the module label into a regular expression.
+      type_ = kAny;
+      moduleLabel_ = glob_to_regex(label);
+      productInstanceName_ = kAny;
+      processName_ = kAny;
+      return;
+    }
+
+    if (underscores >= 3) {
+      // Split the branch name into <product type>_<module label>_<instance name>_<process name>
+      // and convert the glob expressions into regular expressions.
+      // Note that:
+      //   - the <instance name> may be empty;
+      //   - for non-persistable branches, <module label> may contain additional underscores.
+      std::smatch fields;
+      if (std::regex_match(label, fields, kFields)) {
+        type_ = glob_to_regex(fields[1]);
+        moduleLabel_ = glob_to_regex(fields[2]);
+        productInstanceName_ = glob_to_regex(fields[3]);
+        processName_ = glob_to_regex(fields[4]);
+        return;
+      }
+    }
+
+    // Invalid input.
+    throw edm::Exception(edm::errors::Configuration) << "Invalid module label or branch name: \"" << label << "\"";
+  }
+
+  /* ProductNamePattern::match
+   *
+   * Compare a ProductNamePattern object with a ProductDescription object.
+   */
+  bool ProductNamePattern::match(edm::ProductDescription const& product) const {
+    return (std::regex_match(product.friendlyClassName(), type_) and
+            std::regex_match(product.moduleLabel(), moduleLabel_) and
+            std::regex_match(product.productInstanceName(), productInstanceName_) and
+            std::regex_match(product.processName(), processName_));
+  }
+
+  /* productPatterns
+   *
+   * Utility function to construct a vector<edm::ProductNamePattern> from a vector<std::string>.
+   */
+  std::vector<ProductNamePattern> productPatterns(std::vector<std::string> const& labels) {
+    std::vector<ProductNamePattern> patterns;
+    patterns.reserve(labels.size());
+    for (auto const& label : labels)
+      patterns.emplace_back(label);
+    return patterns;
+  }
+
+}  // namespace edm