01dc778b1416bddb593da8cbdf5537d7b9800c24
Chris Jones <chrisdjones15@gmail.com>
2025-12-10 10:53:55 -0600

    Moved SiteLocalConfigService to FWStorage sub-system

--- FWCore/Services/src/SiteLocalConfigService.h
+++ FWCore/Services/src/SiteLocalConfigService.h
@@ -1,5 +1,5 @@
-#ifndef FWCore_Services_SiteLocalConfigService_H
-#define FWCore_Services_SiteLocalConfigService_H
+#ifndef FWStorage_Services_SiteLocalConfigService_H
+#define FWStorage_Services_SiteLocalConfigService_H
 
 ///////////////////////////////////////////////////////////////////////
 //
